c9fddf895d1a9313cc611ef66e34211ac31aeedb
Alexander Ruliov <alexander.ruliov@gmail.com>
2022-01-30 03:35:03 +0300

    fix /help test

--- src/__tests__/commands/help.test.ts
+++ src/__tests__/commands/help.test.ts
@@ -8,59 +8,30 @@ describe('/help', () => {
 
   afterEach(botTest.afterEach);
 
-  [true, false].forEach((isPrivateChat) => {
-    it(
-      // eslint-disable-next-line jest/valid-title
-      isPrivateChat
-        ? 'should reply that it is private chat'
-        : 'should reply with help',
-      async () => {
-        const {
-          handleUpdate,
-          onIdle,
-          popMessages,
-          unixSeconds,
-          user,
-          privateChat,
-          groupChat,
-        } = await botTest.init();
+  it('should reply with help', async () => {
+    const {handleUpdate, onIdle, popMessages, unixSeconds, user, groupChat} =
+      await botTest.init();
 
-        const chat = isPrivateChat ? privateChat : groupChat;
-
-        await handleUpdate(
-          createMessage({
-            user,
-            chat,
-            unixSeconds,
-            text: '/help',
-            isBotCommand: true,
-          }),
-        );
-
-        await onIdle();
-
-        const messages = popMessages();
-
-        expect(messages.length).toBe(isPrivateChat ? 2 : 1);
-
-        const firstMessage = messages[0];
-        const lastMessage = messages[messages.length - 1];
-
-        if (isPrivateChat) {
-          // eslint-disable-next-line jest/no-conditional-expect
-          expect(firstMessage.chatId).toBe(chat.id);
-          // eslint-disable-next-line jest/no-conditional-expect
-          expect(firstMessage.text).toBe(
-            getNoTranslationText(T_`commandsInPrivateWarning`),
-          );
-        }
-
-        expect(lastMessage.chatId).toBe(chat.id);
-        expect(lastMessage.text).toContain(
-          getNoTranslationText(T_`help_start`),
-        );
-        expect(lastMessage.text).toContain(getNoTranslationText(T_`help_end`));
-      },
+    await handleUpdate(
+      createMessage({
+        user,
+        chat: groupChat,
+        unixSeconds,
+        text: '/help',
+        isBotCommand: true,
+      }),
     );
+
+    await onIdle();
+
+    const messages = popMessages();
+
+    expect(messages.length).toBe(1);
+
+    const message = messages[0];
+
+    expect(message.chatId).toBe(groupChat.id);
+    expect(message.text).toContain(getNoTranslationText(T_`help_start`));
+    expect(message.text).toContain(getNoTranslationText(T_`help_end`));
   });
 });
